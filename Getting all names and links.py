89818fc95c5e914bde1da0dfebf5f7b27a183a96
benz3927 <benz3927@gmail.com>
2023-01-01 13:14:32 -0500

    Update Getting all names and links.py

--- Getting all names and links.py
+++ Getting all names and links.py
@@ -9,20 +9,34 @@ def main():
     parsed_code = BeautifulSoup(source_code, "html.parser")
     
     chunks = parsed_code.find_all('li')
-    # for finding links
-    # find_all "a href"
-    names = []
-#     for name in chunks:
-#         name = name.find("a")
-#         names.append(name)
     
-    counter = 0
+    # find all links
+    links = []
+    a = parsed_code.find_all('a', href=True)
+    for link in a:
+        starter = link['href']
+        
+        if starter[0] == '/':
+            link = 'https://www.nhsinform.scot' + starter
+            links.append(link)
+    
+    # 14:335
+    links = links[14:336]
+    print(links)
+
+
+    
+    names = []
     for index in range(35,378):
         item = chunks[index]
+        
+        
         item = item.text
         
         item = re.sub(r'[\r\t\n ]+', ' ', item).strip()
         names.append(item)
+        
+         
 
     for name in names:
         if name == 'Back to top':
@@ -33,13 +47,13 @@ def main():
     for i in range(1,len(names)+1):
         index.append(i)
          
-    disease_dict = {'Disease': names, 'ID': index}
+    disease_dict = {'Disease': names, 'ID': index, 'Links': links}
     # add link to this
            
     names_df = pd.DataFrame(disease_dict) 
         
     names_df.to_csv('NHS_Disease_Data.csv')
-    print(disease_dict)
+    print(names_df)
   
 
 # add link to dictionary
