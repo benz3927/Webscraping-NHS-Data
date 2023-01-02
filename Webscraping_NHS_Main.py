4cf66d4b04ba7de2c0716bc71d71faa74532c3e3
benz3927 <benz3927@gmail.com>
2023-01-02 11:36:51 -0500

    Update Webscraping_NHS_Main.py

--- Webscraping_NHS_Main.py
+++ Webscraping_NHS_Main.py
@@ -1,23 +1,21 @@
-from bs4 import BeautifulSoup
 import requests
-import numpy as np
-import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
+import re
+import matplotlib as plt
 
-# def country_counter(url):
-url = 'https://www.nhsinform.scot/illnesses-and-conditions/diabetes/type-1-diabetes/'
-source_code = requests.get(url).text
-parsed_code = BeautifulSoup(source_code, "html.parser")
+def main():
+    symptom_url = 'https://www.nhsinform.scot/illnesses-and-conditions/cancer/cancer-types-in-adults/acute-lymphoblastic-leukaemia/#symptoms-of-acute-lymphoblastic-leukaemia'
+    source_code = requests.get(symptom_url).text
+    parsed_code = BeautifulSoup(source_code, "html.parser")
 
-everything = parsed_code.find_all('h2')
-print(everything)
+    everything = parsed_code.find_all('p')
 
-# chunk = everything[1].find_all("tr")
-# countries = []
-# 
-# for item in chunk:
-#     item = item.text
-#     item = item.split("\n")
-#     
-#     print(item)
-
-# main()
+# find the first few li bullet points after the symptom header and stop till the next major heading
+    
+    for p in everything:
+        p = p.text
+#         if 'include:' and 'Symptoms' in p:
+#             symptom_header = p
+            
+    print(p)
+main()
