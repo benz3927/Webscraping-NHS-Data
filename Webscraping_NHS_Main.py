426ebae32be25632abaf3e26918e5cc7c0e46e88
benz3927 <benz3927@gmail.com>
2023-01-03 13:27:39 -0500

    Update Webscraping_NHS_Main.py

--- Webscraping_NHS_Main.py
+++ Webscraping_NHS_Main.py
@@ -3,19 +3,36 @@ from bs4 import BeautifulSoup
 import re
 import matplotlib as plt
 
+def finder(everything):
+    for index in range(len(everything)):
+        p = everything[index].text
+        sentence = p.split(' ')
+        if 'include:' and 'Symptoms' in sentence:
+            symptom_header = ' '.join(sentence)
+            return index
+
 def main():
     symptom_url = 'https://www.nhsinform.scot/illnesses-and-conditions/cancer/cancer-types-in-adults/acute-lymphoblastic-leukaemia/#symptoms-of-acute-lymphoblastic-leukaemia'
     source_code = requests.get(symptom_url).text
     parsed_code = BeautifulSoup(source_code, "html.parser")
 
     everything = parsed_code.find_all('p')
+    divs = parsed_code.find_all('div',class='editor')
+    for div in divs:
+        if 'symptoms' in div:
+            symptom_chunk = div
+    print(symptom_chunk.find_all('ul'))
+    
+    
+    uls = parsed_code.find_all('ul')
 
 # find the first few li bullet points after the symptom header and stop till the next major heading
     
-    for p in everything:
-        p = p.text
-#         if 'include:' and 'Symptoms' in p:
-#             symptom_header = p
-            
-    print(p)
+    index = finder(everything)
+#     print(everything[index])
+# index 6 and 7
+
+    print(uls[6])
+    print(uls[7])
+
 main()
