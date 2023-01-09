dbdaefcd5ceaa38a6206e616aeb526fb6e43b1ab
benz3927 <benz3927@gmail.com>
2023-01-09 11:16:30 -0500

    Update Webscraping_NHS_Main.py

--- Webscraping_NHS_Main.py
+++ Webscraping_NHS_Main.py
@@ -12,34 +12,49 @@ def get_symptoms(symptom_url):
     parsed_code = BeautifulSoup(source_code, "html.parser")
     divs = parsed_code.find_all('div',class_="editor")
     
+#     if "symptoms" or "Symptoms" not in h2s:
+#         return 'Symptom is not a bullet point'
     
-    # Generic method for all symptom bullet points
+    
+     # Generic method for all symptom bullet points
+     # have to find bullets directly under a paragraph
+     
     if len(divs) > 1:
-        for div in divs:
-            h2 = div.find('h2')
-            if 'symptoms' in h2.text:
-                chunk_div = div
-        
-        symptoms_chunk = chunk_div.find_all('ul')
-#                 print(symptoms_chunk)
-        all_symptoms = []
-
-        for symptoms in symptoms_chunk:
-            for symptom in symptoms:
-                symptom= symptom.text
-                symptom = re.sub(r'[\r\t\n ]+', ' ',symptom).strip()
-                all_symptoms.append(symptom)
-        
         symptoms_list = []
-        for item in all_symptoms:
-            if item != '':
-                symptoms_list.append(item)
-            
-        return symptoms_list
+        
+        for div in divs:
+            ps = div.find_all('p')
+            p_list = []
+            for p in ps:
+                p_list.append(p.text)
+            words = []
+            for sentence in p_list:
+                sentence = sentence.split(' ')
+                for word in sentence:
+                    words.append(word)
+                
+                if 'include:' in words:
+                    chunk_div = div
+        
+                    symptoms_chunk = chunk_div.find_all('ul')
+            #                 print(symptoms_chunk)
+                    
+                    for symptoms in symptoms_chunk:
+                        for symptom in symptoms:
+                            symptom = symptom.text
+                            symptom = re.sub(r'[\r\t\n ]+', ' ',symptom).strip()
+                            symptoms_list.append(symptom)
+                    
+                    updated_symptoms_list = []
+                    for item in symptoms_list:
+                        if item != '':
+                            updated_symptoms_list.append(item)
+                    
+                    symptoms = updated_symptoms_list
+                    return symptoms
     
     else:
         return 'null'
-
 
 def main():
     # Setup the csv
@@ -106,9 +121,9 @@ def main():
     
     for index in range(len(disease_symptoms)):
 
-        if 'null' in disease_symptoms[index]:
-
-
+        if disease_symptoms[index] is None:
+            all_names.append(names[index])
+            all_indices.append(index+1)
             all_symptoms.append('null')
             
         else:
@@ -120,22 +135,15 @@ def main():
             
             for symptom in disease_symptoms[index]:
                 all_symptoms.append(symptom)
-        
-        
-            
-        
-            
-        
-            
-                
     
-#     
+
 #     disease_symptoms_dict = {'Disease': all_names, 'ID': all_indices, 'Symptoms': all_symptoms}
-#     
 #     all_disease_symptoms = pd.DataFrame(disease_symptoms_dict) 
-#         
 #     all_disease_symptoms.to_csv('NHS_Disease_Symptoms.csv')
             
 
 
+
 main()
+
+
