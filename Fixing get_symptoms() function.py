d7ac9f17d75cffce9742a1e70b55dfee751b28cc
benz3927 <benz3927@gmail.com>
2023-01-08 22:55:00 -0500

    Update Fixing get_symptoms() function.py

--- Fixing get_symptoms() function.py
+++ Fixing get_symptoms() function.py
@@ -12,36 +12,52 @@ def get_symptoms(symptom_url):
     parsed_code = BeautifulSoup(source_code, "html.parser")
     divs = parsed_code.find_all('div',class_="editor")
     
+#     if "symptoms" or "Symptoms" not in h2s:
+#         return 'Symptom is not a bullet point'
     
-    # Generic method for all symptom bullet points
+    
+     # Generic method for all symptom bullet points
+     # have to find bullets directly under a paragraph
+     
     if len(divs) > 1:
-        for div in divs:
-            h2 = div.find('h2')
-            if 'symptoms' in h2.text:
-                symptoms_chunk = div
-        
-        symptoms_chunk = symptoms_chunk.find_all('ul')
-#                 print(symptoms_chunk)
-        all_symptoms = []
-
-        for symptoms in symptoms_chunk:
-            for symptom in symptoms:
-                symptom= symptom.text
-                symptom = re.sub(r'[\r\t\n ]+', ' ',symptom).strip()
-                all_symptoms.append(symptom)
-        
         symptoms_list = []
-        for item in all_symptoms:
-            if item != '':
-                symptoms_list.append(item)
-            
-        return symptoms_list
+        
+        for div in divs:
+            ps = div.find_all('p')
+            p_list = []
+            for p in ps:
+                p_list.append(p.text)
+            words = []
+            for sentence in p_list:
+                sentence = sentence.split(' ')
+                for word in sentence:
+                    words.append(word)
+                
+                if 'include:' in words:
+                    chunk_div = div
+        
+                    symptoms_chunk = chunk_div.find_all('ul')
+            #                 print(symptoms_chunk)
+                    
+                    for symptoms in symptoms_chunk:
+                        for symptom in symptoms:
+                            symptom = symptom.text
+                            symptom = re.sub(r'[\r\t\n ]+', ' ',symptom).strip()
+                            symptoms_list.append(symptom)
+                    
+                    updated_symptoms_list = []
+                    for item in symptoms_list:
+                        if item != '':
+                            updated_symptoms_list.append(item)
+                    
+                    symptoms = updated_symptoms_list
+                    return symptoms
     
     else:
         return 'null'
 
 def main():
-    print(get_symptoms('https://www.nhsinform.scot/illnesses-and-conditions/stomach-liver-and-gastrointestinal-tract/acute-pancreatitis/'))
+    print(get_symptoms('https://www.nhsinform.scot/illnesses-and-conditions/heart-and-blood-vessels/conditions/abdominal-aortic-aneurysm/'))
     
 
 
